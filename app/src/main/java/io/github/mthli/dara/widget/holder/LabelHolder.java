78072b778b091bd71c8eab56818d591bdfc3ee7b
Matthew Lee <matthewlee0725@gmail.com>
2016-05-28 14:32:11 +0800

    update

--- app/src/main/java/io/github/mthli/dara/widget/holder/LabelHolder.java
+++ app/src/main/java/io/github/mthli/dara/widget/holder/LabelHolder.java
@@ -1,4 +1,20 @@
 package io.github.mthli.dara.widget.holder;
 
-public class LabelHolder {
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import io.github.mthli.dara.R;
+
+public class LabelHolder extends RecyclerView.ViewHolder {
+    private AppCompatTextView mLabel;
+
+    public LabelHolder(View view) {
+        super(view);
+        mLabel = (AppCompatTextView) view.findViewById(R.id.label);
+    }
+
+    public void setText(CharSequence label) {
+        mLabel.setText(label);
+    }
 }
--- app/src/main/java/io/github/mthli/dara/widget/holder/PackageHolder.java
+++ app/src/main/java/io/github/mthli/dara/widget/holder/PackageHolder.java
@@ -1,4 +1,32 @@
 package io.github.mthli.dara.widget.holder;
 
-public class PackageHolder {
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatImageButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import io.github.mthli.dara.R;
+
+public class PackageHolder extends RecyclerView.ViewHolder {
+    private AppCompatTextView mPkg;
+    private AppCompatImageButton mAction;
+
+    public PackageHolder(View view) {
+        super(view);
+        mPkg = (AppCompatTextView) view.findViewById(R.id.pkg);
+        mAction = (AppCompatImageButton) view.findViewById(R.id.action);
+    }
+
+    public void setIcon(Drawable drawable) {
+        mPkg.setCompoundDrawables(drawable, null, null, null);
+    }
+
+    public void setName(CharSequence name) {
+        mPkg.setText(name);
+    }
+
+    public void setAction(View.OnClickListener listener) {
+        mAction.setOnClickListener(listener);
+    }
 }
