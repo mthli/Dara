92b84be3faae174807e0a32a06c04a04a80bf9ce
Matthew Lee <matthewlee0725@gmail.com>
2016-06-04 22:36:10 +0800

    update

--- app/src/main/java/io/github/mthli/dara/widget/RuleContentLayout.java
+++ app/src/main/java/io/github/mthli/dara/widget/RuleContentLayout.java
@@ -5,11 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 
 import io.github.mthli.dara.R;
 
-public class RuleContentLayout extends LinearLayout {
+public class RuleContentLayout extends LinearLayout
+        implements CompoundButton.OnCheckedChangeListener {
     private SwitchCompat mSwitchRegular;
     private AppCompatEditText mTitleView;
     private AppCompatEditText mContentView;
@@ -39,6 +41,18 @@ public class RuleContentLayout extends LinearLayout {
         mSwitchRegular = (SwitchCompat) findViewById(R.id.switch_regular);
         mTitleView = (AppCompatEditText) findViewById(R.id.title);
         mContentView = (AppCompatEditText) findViewById(R.id.content);
+
+        mSwitchRegular.setOnCheckedChangeListener(this);
+        mTitleView.setHint(R.string.hint_title_separator);
+        mContentView.setHint(R.string.hint_content_separator);
         setCustomEnable(false);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        mTitleView.setHint(isChecked ? R.string.hint_title_regular
+                : R.string.hint_title_separator);
+        mContentView.setHint(isChecked ? R.string.hint_content_regular
+                : R.string.hint_content_separator);
     }
 }
