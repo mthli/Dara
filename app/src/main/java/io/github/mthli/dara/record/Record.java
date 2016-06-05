4c07ad04404e4aa6f016398151b86fc79978d085
Matthew Lee <matthewlee0725@gmail.com>
2016-06-05 15:54:16 +0800

    update

--- app/src/main/java/io/github/mthli/dara/record/Record.java
+++ app/src/main/java/io/github/mthli/dara/record/Record.java
@@ -1,7 +1,53 @@
 package io.github.mthli.dara.record;
 
 import com.orm.SugarRecord;
+import com.orm.dsl.Table;
 
+@Table
 public class Record extends SugarRecord {
-    // TODO
+    private String mPackageName;
+    private Boolean mIsRegularExpression;
+    private String mTitle;
+    private String mContent;
+
+    public Record() {}
+
+    public Record(String packageName, Boolean isRegularExpression, String title, String content) {
+        mPackageName = packageName;
+        mIsRegularExpression = isRegularExpression;
+        mTitle = title;
+        mContent = content;
+    }
+
+    public String getPackageName() {
+        return mPackageName;
+    }
+
+    public void setPackageName(String packageName) {
+        mPackageName = packageName;
+    }
+
+    public Boolean getRegularExpression() {
+        return mIsRegularExpression;
+    }
+
+    public void setRegularExpression(Boolean regularExpression) {
+        mIsRegularExpression = regularExpression;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
 }
