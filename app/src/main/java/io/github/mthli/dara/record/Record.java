3a9d8329bcfbb4966c675e97677c6c92711cb2b7
Matthew Lee <matthewlee0725@gmail.com>
2016-06-05 15:56:41 +0800

    update

--- app/src/main/java/io/github/mthli/dara/record/Record.java
+++ app/src/main/java/io/github/mthli/dara/record/Record.java
@@ -1,19 +1,24 @@
 package io.github.mthli.dara.record;
 
+import android.graphics.Bitmap;
+
 import com.orm.SugarRecord;
 import com.orm.dsl.Table;
 
 @Table
 public class Record extends SugarRecord {
     private String mPackageName;
+    private Bitmap mSmallIcon;
     private Boolean mIsRegularExpression;
     private String mTitle;
     private String mContent;
 
     public Record() {}
 
-    public Record(String packageName, Boolean isRegularExpression, String title, String content) {
+    public Record(String packageName, Bitmap smallIcon, Boolean isRegularExpression,
+                  String title, String content) {
         mPackageName = packageName;
+        mSmallIcon = smallIcon;
         mIsRegularExpression = isRegularExpression;
         mTitle = title;
         mContent = content;
@@ -25,6 +30,14 @@ public class Record extends SugarRecord {
 
     public void setPackageName(String packageName) {
         mPackageName = packageName;
+    }
+
+    public Bitmap getSmallIcon() {
+        return mSmallIcon;
+    }
+
+    public void setSmallIcon(Bitmap smallIcon) {
+        mSmallIcon = smallIcon;
     }
 
     public Boolean getRegularExpression() {
